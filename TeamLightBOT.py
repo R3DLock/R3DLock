9b3a880d713f1e9859aaa35dc9814fe7a3fbd406
R3DLock <47334859+R3DLock@users.noreply.github.com>
2019-02-25 20:14:53 +0100

    Update TeamLightBOT.py

--- TeamLightBOT.py
+++ TeamLightBOT.py
@@ -153,4 +153,4 @@ async def cu(ctx):
     await ctx.message.delete()
 
 
-bot.run(bot.run(os.environ('NTQxNjU3NTg5NjAwMjIzMjM0.DziuZQ.AlTHzoR1jXwgumjjkuTME3FQojs'))) #Lancer le bot. Remplacez token par votre token et laissez les apostrophes
+bot.run(bot.run(os.environ('CECI EST UN TOKEN'))) #Lancer le bot. Remplacez token par votre token et laissez les apostrophes
